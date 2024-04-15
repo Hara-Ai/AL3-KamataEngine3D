68fb85d89d289d5146165fd6f4674ed0af127122
GC1B_12_ハラ_アイ <147122028+Hara-Ai@users.noreply.github.com>
2024-04-15 15:16:24 +0900

    タイトルバーに1_1と付けた。

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2A_14_ハラ_アイ");
+	win->CreateGameWindow(L"GC2A_14_ハラ_アイ_1_1");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
