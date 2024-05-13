a51b1a43389b2e3f0e2b7e003ac6dbb7450e4ce8
GC1B_12_ハラ_アイ <147122028+Hara-Ai@users.noreply.github.com>
2024-05-13 17:06:25 +0900

    アフィン変換の関数を作成途中で終わり、またDrowのエラーが出た状態で終わった。天球のヘッダーファイルの作成も行った。

--- DirectXGame/3d/WorldTransform.cpp
+++ DirectXGame/3d/WorldTransform.cpp
@@ -1,13 +1,12 @@
 #include "WorldTransform.h"
 
-void WorldTransform::UpdateMatrix()
-{ matWorld_; }
+void WorldTransform::UpdateMatrix() {matWorld_;{} }
 
 Vector3 WorldTransform::MakeAffineMatrix(Vector3 scale_, Vector3 rotation_, Vector3 translation_)
 {
-	matWorld_ = MakeAffineMatrix(scale_, rotation_, translation_);
+	Vector3 matWorld_ = MakeAffineMatrix(scale_, rotation_, translation_);
 
-	TransferMatrix;
-	return Vector3();
+	TransferMatrix();
+	return Vector3(matWorld_);
 }
 
