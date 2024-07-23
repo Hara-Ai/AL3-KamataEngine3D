166d51b5ee4ffaf685c5f0c3648f16ff495c93f0
GC1B_12_ハラ_アイ <147122028+Hara-Ai@users.noreply.github.com>
2024-07-23 17:33:47 +0900

    更新

--- DirectXGame/MapChipField.h.cpp
+++ DirectXGame/MapChipField.h.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿#include "MapChipField.h.h"
 #include <map>
 #include <fstream>
@@ -79,3 +80,111 @@ Vector3 MapChipField::GetMapChipPositionByIndex(uint32_t xIndex, uint32_t yIndex
 }
 
 
+=======
+﻿//#include "MapChipField.h"
+//#include <map>
+//#include <fstream>
+//#include <sstream>
+//#include <cassert>
+//
+//std::map<std::string, MapChipType> mapChipTable = 
+//{
+//	{"0", MapChipType::kBlank},
+//    {"1", MapChipType::kBlock},
+//};
+//
+//void MapChipField::ResetMapChipData() 
+//{
+//	mapChipData_.data.clear();
+//	mapChipData_.data.resize(kNumBockVirtical);
+//	for (std::vector<MapChipType>& mapChipDataLine : mapChipData_.data) {
+//		mapChipDataLine.resize(kNumBockHorizontal);
+//	}
+//};
+//
+//void MapChipField::LoadMapChipCsv(const std::string& filePath) 
+//{
+//
+//	//マップチップデータをリセット
+//	ResetMapChipData();
+//
+//	//ファイルを開く
+//	std::ifstream file;
+//	file.open(filePath);
+//	assert(file.is_open());
+//
+//	//マップチップCSV(宣言)
+//	std::stringstream mapChipCsv;
+//	//ファイルの内容を文字列ストリームにコピー
+//	mapChipCsv << file.rdbuf();
+//	//ファイルを閉じる
+//	file.close();
+//
+//	
+//
+//	for (uint32_t i = 0; i < kNumBockVirtical; ++i)
+//	{
+//		std::string line;
+//		getline(mapChipCsv, line);
+//
+//		std::istringstream line_stream(line);
+//
+//		for (uint32_t j = 0; j < kNumBockHorizontal; ++j)
+//		{
+//			std::string word;
+//			getline(line_stream, word, ',');
+//
+//			if (mapChipTable.contains(word))
+//			{
+//				mapChipData_.data[i][j] = mapChipTable[word];
+//			}
+//		}
+//	}
+//
+//}
+//
+//MapChipType MapChipField::GetMapChipTypeByIndex(uint32_t xIndex, uint32_t yIndex) 
+//{
+//	if (xIndex < 0 || kNumBockHorizontal - 1 < xIndex)
+//	{
+//		return MapChipType::kBlank;
+//	}
+//
+//	if (xIndex < 0 || kNumBockVirtical - 1 < yIndex) {
+//		return MapChipType::kBlank;
+//	}
+//
+//	return mapChipData_.data[yIndex][xIndex]; 
+//}
+//
+//Vector3 MapChipField::GetMapChipPositionByIndex(uint32_t xIndex, uint32_t yIndex) 
+//{
+//	return Vector3(kBockWidth * xIndex,kBockHeight * (kNumBockVirtical - 1 -yIndex),0); 
+//}
+//
+//IndexSet MapChipField::GetMapChipIndexSetByPoition(const Vector3& position)
+//{
+//	IndexSet indexSet = {};
+//
+//
+//	float x = ((position.x + kBockWidth / 2)/ kBockHeight);
+//	float y = ((position.y + kBockWidth / 2)/ kBockHeight);
+//	indexSet.xIndex = kNumBockVirtical - 1 - x;
+//	indexSet.yIndex = kNumBockVirtical - 1 - y;
+//	return indexSet;
+//}
+//
+//cameraRect MapChipField::GetRectByIndex(uint32_t xIndex, uint32_t yIndex) 
+//{
+//	// 指定ブロックの中心座標を取得する
+//	Vector3 center = GetMapChipPositionByIndex(xIndex, yIndex);
+//
+//	cameraRect rect;
+//	rect.left = center.x - kBlockWidth / 2.0f;
+//	rect.right = center.x - kBlockWidth / 2.0f;
+//	rect.bottom = center.y - kBlockHeight / 2.0f;
+//	rect.top = center.y - kBlockHeight / 2.0f;
+//	return rect;
+//}
+//
+>>>>>>> Stashed changes
