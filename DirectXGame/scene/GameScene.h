b7998e52f557eb343c0a325232b54c9941b71513
GC1B_12_ハラ_アイ <147122028+Hara-Ai@users.noreply.github.com>
2024-08-05 17:51:59 +0900

    GameScene復活。

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -1,12 +1,20 @@
 #pragma once
-
+#include "MapChipField.h"
 #include "Audio.h"
 #include "DirectXCommon.h"
+#include "Skydome.h"
 #include "Input.h"
 #include "Model.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
+#include "Player.h"
+#include "Enemy.h"
+#include "CameraController.h"
+#include <cassert>
+#include "DeathParticles.h"
+
 
 /// <summary>
 /// ゲームシーン
@@ -14,6 +22,7 @@
 class GameScene {
 
 public: // メンバ関数
+
 	/// <summary>
 	/// コンストクラタ
 	/// </summary>
@@ -23,6 +32,8 @@ public: // メンバ関数
 	/// デストラクタ
 	/// </summary>
 	~GameScene();
+
+	void ChecAllCollisiions();
 
 	/// <summary>
 	/// 初期化
@@ -39,12 +50,50 @@ public: // メンバ関数
 	/// </summary>
 	void Draw();
 
+	void GenerateBlocks();
+	bool IsCollision(AABB aabb1, AABB aabb2);
+
 private: // メンバ変数
+
+	WorldTransform worldTransform_;
 	DirectXCommon* dxCommon_ = nullptr;
-	Input* input_ = nullptr;
+	Input* input_ = nullptr; 
 	Audio* audio_ = nullptr;
+	Model* modelBlock_ = nullptr;
+	Model* modelSkydome_ = nullptr;
+	MapChipField* mapChipField_;
+	Skydome* skydome_ = nullptr;
+	Player* player_ = nullptr;
+	Enemy* enemy_ = nullptr;
+	Model* model_ = nullptr;
+	Model* enmeyModel_ = nullptr;
+	CameraController* CameraController_ = nullptr;
+
+	std::vector<std::vector<WorldTransform*>> worldTransformBlocks_;
+	WorldTransform wolrldTransform_ = {};
+	ViewProjection viewProjection_ = {}; 
+	MapChipData mapChipData_ = {};
+	uint32_t numBlockVirtical_ = 0;
+	uint32_t numBlockHorizontal_ = 0;
+	uint32_t tetureHandle_ = 0;
+	
+	std::list<Enemy*> enemies_;
 
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	
+	// デバックカメラ有効
+	bool isDebugCameraActiive_ = false;
+
+	//デバックカメラ
+	DebugCamera* debugCamera_ = nullptr;
+
+	//デバックカメラのビュープロジェクション
+	ViewProjection* debugViewProjection_; 
+
+	DeathParticles* deathParticles_ ;
+	//デスパーティクルが存在するか
+	bool deathParticlesFlag = false;
+
 };
