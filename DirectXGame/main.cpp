f02a81389117a3da7bbb333d117f52bfb04fd74c
KoyamaShota <k023g1039@g.neec.ac.jp>
2024-09-09 14:59:52 +0900

    タイトルを変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2A_14_ハラ_アイ");
+	win->CreateGameWindow(L"6022_半透明少年");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
